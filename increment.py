fa914d91170a7d6531ceaf4b361185a9c635a322
Jesse Aldridge <jesse.aldridge@airbnb.com>
2018-03-25 17:42:31 -0700

    regex so no need for _

--- increment.py
+++ increment.py
@@ -8,10 +8,13 @@ paths = os.listdir(dir_path)
 paths_with_numbered_filenames = sorted([path for path in paths if re.match('^[0-9]+', path)])
 
 def increment_path(path, value):
-  num_str, base_path = os.path.basename(path).split('_', 1)
+  match = re.match('^([0-9]+)(.+)', path)
+  if match is None:
+    return
+  num_str, base_path = match.group(1), match.group(2)
   num = int(num_str)
   num += value
-  new_path = '{}_{}'.format(str(num).zfill(2), base_path)
+  new_path = '{}{}'.format(str(num).zfill(2), base_path)
   shutil.move(path, new_path)
 
 for path in paths_with_numbered_filenames[pivot:]:
